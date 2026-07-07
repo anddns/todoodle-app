a80ded1dbfd2ac8843fac4a707bc2a3b316c44ab
Anderson N. Silva <anddns92@gmail.com>
2026-07-07 19:31:12 -0300

    Adição da dependência '@elysiajs/cors' à API da aplicação.

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,7 @@
+import { cors } from '@elysiajs/cors'
 import { Elysia } from 'elysia'
-
 import { routes } from '@/api/routes'
+
 import { globalErrorHandler } from '@/api/shared/errors/global-error-handler'
 
-export const app = new Elysia().use(globalErrorHandler).use(routes)
+export const app = new Elysia().use(globalErrorHandler).use(cors()).use(routes)
