1783b2de8a479046bc64dc3058f327df1b418ad5
Anderson N. Silva <anddns92@gmail.com>
2026-07-07 10:08:34 -0300

    Alteração nas configurações da instância Drizzle ORM

--- apps/api/src/shared/database/index.ts
+++ apps/api/src/shared/database/index.ts
@@ -5,6 +5,6 @@ import { Pool } from 'pg'
 import { env } from '@/api/shared/settings/env'
 
 export const pool = new Pool({ connectionString: env.DB_URL })
-export const database = drizzle({ client: pool })
+export const database = drizzle({ client: pool, logger: false })
 
 export type DrizzleDatabase = typeof database
