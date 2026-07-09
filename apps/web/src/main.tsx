07e0bfb2d33a5eb9d5eb2da56ca981d65e30957e
Anderson N. Silva <anddns92@gmail.com>
2026-07-08 23:54:23 -0300

    Integração de nova configuração do QueryClient ao roteador

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,21 +1,20 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
+import { queryClient } from './lib/query-client'
 import { routeTree } from './routeTree.gen'
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, context: { queryClient }, defaultPreload: 'intent' })
 
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
   }
 }
-
-const queryClient = new QueryClient()
 
 const rootElement = document.getElementById('root')!
 
