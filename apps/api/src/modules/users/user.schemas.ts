12c4fb1ba9430d594379715a010347129fa517bf
Anderson N. Silva <anddns92@gmail.com>
2026-07-17 01:07:33 -0300

    feat(api): remover esquemas 'user' não utilizados e otimizar a lógica de validação de usuário

--- apps/api/src/modules/users/user.schemas.ts
+++ apps/api/src/modules/users/user.schemas.ts
@@ -12,14 +12,6 @@ export const userSchema = z.object({
   createdAt: z.iso.datetime(),
 })
 
-export const publicUserSchema = z.object({
-  email: z.email().min(1),
-  name: z.string(),
-  isVerified: z.boolean(),
-  updatedAt: z.iso.datetime(),
-  createdAt: z.iso.datetime(),
-})
-
 export const userParamsSchema = z.object({
   id: z.uuidv7(),
 })
@@ -32,27 +24,6 @@ export const listUsersQuerySchema = z.object({
   offset: z.coerce.number().int().min(0).default(0),
 })
 
-export const createUserSchema = z.object({
-  email: z.email().min(1),
-  name: z.string(),
-  password: z.string(),
-  verifiedAt: z.iso.datetime().optional(),
-})
-
-export const updateUserSchema = z
-  .object({
-    email: z.email().min(1).optional(),
-    name: z.string().optional(),
-    password: z.string().optional(),
-    verifiedAt: z.iso.datetime().nullable().optional(),
-  })
-  .refine((data) => Object.values(data).some((value) => value !== undefined), {
-    error: 'At least one of the properties must be provided.',
-  })
-
 export type User = typeof usersTable.$inferSelect
-export type PublicUser = z.infer<typeof publicUserSchema>
 export type UserParamsInput = z.infer<typeof userParamsSchema>
 export type ListUsersQuery = z.infer<typeof listUsersQuerySchema>
-export type CreateUserInput = z.infer<typeof createUserSchema>
-export type UpdateUserInput = z.infer<typeof updateUserSchema>
