a01c886c7de25ba3451dcf6791a7f821d4e7d7e2
Anderson N. Silva <anddns92@gmail.com>
2026-07-18 00:27:02 -0300

    fix(api): atualizar o tipo de retorno da exportação 'Task' para usar 'tasks' em vez de 'data'

--- apps/web/src/features/tasks/types/index.ts
+++ apps/web/src/features/tasks/types/index.ts
@@ -4,6 +4,6 @@ import type { eden } from '@/web/lib/eden'
 type TasksListEndpoint = ReturnType<typeof eden.api.v1.tasks.get>
 type TaskByIdEndpoints = ReturnType<typeof eden.api.v1.tasks>
 
-export type Task = Treaty.Data<TasksListEndpoint>['data'][number]
+export type Task = Treaty.Data<TasksListEndpoint>['tasks'][number]
 export type CreateTaskPayload = Parameters<typeof eden.api.v1.tasks.post>[0]
 export type UpdateTaskPayload = NonNullable<Parameters<TaskByIdEndpoints['patch']>[0]>
